9015969221837857158cb1b03a00faaf3158e181
Lucas da Cunha <luques.cunha@gmail.com>
2024-07-20 23:06:20 -0300

    Removendo injeções de dependências não utilizadas

--- RestauranteService/ItemServiceHttpClient/ItemServiceHttpClient.cs
+++ RestauranteService/ItemServiceHttpClient/ItemServiceHttpClient.cs
@@ -1,20 +1,14 @@
 ﻿using RestauranteService.Dtos;
 using RestauranteService.RabbitMqClient;
-using System.Text;
-using System.Text.Json;
 
 namespace RestauranteService.ItemServiceHttpClient
 {
     public class ItemServiceHttpClient : IItemServiceHttpClient
     {
-        private readonly HttpClient _client;
-        private readonly IConfiguration _configuration;
         private readonly IRabbitMqClient _rabbitMqClient;
 
-        public ItemServiceHttpClient(HttpClient client, IConfiguration configuration, IRabbitMqClient rabbitMqClient)
+        public ItemServiceHttpClient(IRabbitMqClient rabbitMqClient)
         {
-            _client = client;
-            _configuration = configuration;
             _rabbitMqClient = rabbitMqClient;
         }
 
